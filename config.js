62a4c79ea771f8e5dfacae4b971be3646b007068
Burhan Yildiz <brhnyldz007@gmail.com>
2021-03-14 20:42:47 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '450150063006879',
+        clientSecret: '8b87fd2b02c717edd5a0db732230bb07'
+    }
+}
